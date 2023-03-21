a5653e50d9910ba5348a813b383874952300cf4a
L-Grim <111025216+L-Grim@users.noreply.github.com>
2023-03-21 19:46:22 -0300

    Add files via upload

---
+++ Aula_21.03.23/Matrix_de_numeros_reverso/src/Revisao.java
@@ -0,0 +1,20 @@
+
+public class Revisao {
+
+	public static void main(String[] args) {
+	
+		int[] egArray = {2, 4, 6, 8, 10, 1, 3, 5, 7, 9};
+	
+		for(int index = egArray.length-1; index >= 0 ; index--) {
+		
+			System.out.println(egArray[index] + " ");
+		
+		}
+
+		double teste = (1 / 2 + 3.5) * 2;
+		
+		System.out.println(teste);
+		
+	}
+
+}
---
+++ Aula_21.03.23/matriz_de_numeros/src/matriz_de_numeros/Revisao.java
@@ -0,0 +1,17 @@
+package matriz_de_numeros;
+
+public class Revisao {
+	
+	public static void main(String[] args) {
+		
+		int[] egArray = {2, 4, 6, 8, 10, 1, 3, 5, 7, 9};
+		
+		for(int index = 0; index < egArray.length; index++) {
+			
+			System.out.println(egArray[index] + " ");
+			
+		}
+		
+	}
+
+}
