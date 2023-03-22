9b8f4bb01571093803c794ba97cac48bfb8fdb10
L-Grim <111025216+L-Grim@users.noreply.github.com>
2023-03-22 20:03:52 -0300

    Add files via upload

---
+++ Aula_22.03.23/Enumeracao/src/Enum.java
@@ -0,0 +1,27 @@
+
+public class Enum {
+
+	enum Semana {Segunda, Terça, Quarta, Quinta, Sexta, Sabado, Domingo};
+	
+	public static void main(String[] args) {
+		
+		Semana dia1 = Semana.Segunda;
+		Semana dia2 = Semana.Terça;
+		Semana dia3 = Semana.Quarta;
+		Semana dia4 = Semana.Quinta;
+		Semana dia5 = Semana.Sexta;
+		Semana dia6 = Semana.Sabado;
+		Semana dia7 = Semana.Domingo;
+		
+		System.out.println(Semana.Segunda);
+		System.out.println(Semana.Terça);
+		System.out.println(Semana.Quarta);
+		System.out.println(Semana.Quinta);
+		System.out.println(Semana.Sexta);
+		System.out.println(Semana.Sabado);
+		System.out.println(Semana.Domingo);
+		
+	}
+	
+}	
+	
---
+++ Aula_22.03.23/contando/src/contando/muito.java
@@ -0,0 +1,24 @@
+package contando;
+
+public class muito {
+
+	public static void main(String[] args) {
+		
+		int x = 0;
+		
+		while(x <= 100) {
+			
+			System.out.println("x = " + x);
+			x++;
+			
+		}
+		
+		for(int i = 0; i <= 100; i++) {
+			
+			System.out.println("i = " + i);
+			
+		}
+	
+	}
+
+}
